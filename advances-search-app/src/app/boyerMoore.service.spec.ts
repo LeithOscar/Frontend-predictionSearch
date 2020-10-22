fc95fa05eab9670f1da5215ee90299077addd910
Leith Oscar Jarayseh Gutierrez <leith.jarayseh@wolterskluwer.com>
2020-10-22 10:54:41 +0200

    boyer Moore impl

--- advances-search-app/src/app/boyerMoore.service.spec.ts
+++ advances-search-app/src/app/boyerMoore.service.spec.ts
@@ -13,14 +13,70 @@ describe('boyerMoorePatternService', () => {
 
     describe('find the word on text', () => {
 
-        it('find', () => {
-            const searchText = 'word';
-            const text = 'there would have been a time for such a word';
+        describe('finbruteForced', () => {
+
+            it('finbruteForced', () => {
+                const searchText = 'ostente';
+                const text = 'la vecina ostente';
+                const exist = service.bruteForce(text, searchText);
+
+                expect(exist).toBe(1);
+            });
 
 
-            const exist = service.boyerMooreSearch(text, searchText);
-
-            expect(exist).toBe(1);
         });
+
+        describe('badMatchTable', () => {
+            it('find test on text', () => {
+                const searchText = 'test';
+                const text = 'this is a test';
+                const exist = service.badMatchTable(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+
+            it('find text on text', () => {
+                const searchText = 'text';
+                const text = 'alltexttogether';
+                const exist = service.badMatchTable(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+
+            it('find Visa on text', () => {
+                const searchText = 'Visa';
+                const text = 'paywithVisa';
+                const exist = service.badMatchTable(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+
+            it('find 97 on text', () => {
+                const searchText = '97';
+                const text = 'nº64986xxx - num97';
+                const exist = service.badMatchTable(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+
+            it('find Ñ on text', () => {
+                const searchText = '97';
+                const text = 'nº6498ñ6xxx - num97';
+                const exist = service.badMatchTable(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+
+            it('find $ on text', () => {
+                const searchText = '97';
+                const text = 'pay 9.099$';
+                const exist = service.badMatchTable(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+        });
+
+
+
     });
 });
--- advances-search-app/src/app/boyerMoore.service.ts
+++ advances-search-app/src/app/boyerMoore.service.ts
@@ -1,37 +1,107 @@
 import { Injectable } from '@angular/core';
-import { DepthwiseConv2dNativeBackpropInput } from '@tensorflow/tfjs';
-import { element } from 'protractor';
+import { math, Max } from '@tensorflow/tfjs';
+
 
 @Injectable()
 export class BoyerMoorePatternService {
-    d1: [] = [];
+    private d1: [] = [];
+    private SPACE = ' ';
+    patternLength: number;
+    pattern: string;
+    text: string;
 
-    boyerMooreSearch(text: string, pattern: string): number {
-
-
+    bruteForce(text: string, pattern: string): number {
         if (pattern.length === 0) {
             return -1;
         }
 
-        this.tableD1(pattern);
+        this.patternLength = pattern.length;
+        this.pattern = pattern;
+        this.text = text;
+        return this.search();
     }
 
-    tableD1(pattern: string): void {
+    search(): number {
 
-        const items = Array.from(pattern.replace(/(.)(?=.*\1)/g, '')); // distinct
-        items.forEach((element) => {
-            this.d1[element] = this.getPositionOnPattern(element, pattern);
-        });
-    }
+        // remember, read from right to left
+        const lengthOfPattern = this.patternLength;
+        const lengthOfText = this.text.length;
 
-    getPositionOnPattern(char: string, pattern: string): number {
-        const iterator = Array.from(pattern);
-        for (let index = pattern.length; index <= iterator.length; index--) { // read to right to left
-            if (char === iterator[index]) {
-                return pattern.indexOf(iterator[index]);  // return first element found
+
+        for (let i = 0; i <= lengthOfText - lengthOfPattern; i++) {
+            let j = 0;
+
+            for (j = 0; j < lengthOfPattern; j++) {
+                if (this.text.charAt(i + j) !== this.pattern.charAt(j)) {
+                    break;
+                }
             }
+
+            if (j === lengthOfPattern) {
+                // return i; position of word
+                return 1;
+            }
+
+        }
+
+        return 0;
+
+    }
+
+
+    badMatchTable(text: string, pattern: string): number {
+        if (pattern.length === 0) {
+            return -1;
+        }
+
+        this.patternLength = pattern.length;
+        this.pattern = pattern;
+        this.text = text;
+        this.tableD1(); // bad macht table
+        return this.searchBadTable();
+    }
+
+    searchBadTable(): number {
+
+        let numOfskips = 0;
+
+        for (let i = 0; i <= this.text.length - this.patternLength; i += numOfskips) {
+
+            numOfskips = 0;
+
+            for (let j = this.patternLength - 1; j >= 0; j--) {
+
+                if (this.pattern.charAt(j) !== this.text.charAt(i + j)) {
+
+                    if (this.d1[this.pattern.charAt(i + j)]) {
+
+                        numOfskips = this.d1[this.pattern.charAt(i + j)];
+                        break;
+
+                    } else {
+                        numOfskips = j + 1;
+                    }
+
+                }
+                if (numOfskips === 0) {
+                    // return i;
+                    return 1;
+                }
+            }
+            // return 0;
         }
         return 0;
     }
 
+
+    // create table wrong prefix 'prefijo malo'
+    tableD1(): void {
+
+        for (let index = 0; index < this.patternLength; index++) {
+            const actualCharacter = this.pattern.charAt(index);
+            const maxShift = Math.max(1, this.patternLength - index - 1);
+            this.d1[actualCharacter] = maxShift;
+        }
+        this.d1[this.SPACE] = this.patternLength; // other block
+    }
 }
