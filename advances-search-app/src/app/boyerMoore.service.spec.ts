3a2dd570f53a1642e7c2f186b5671c733652d820
Leith Oscar Jarayseh Gutierrez <leith.jarayseh@wolterskluwer.com>
2020-10-22 11:31:53 +0200

    add casuistics

--- advances-search-app/src/app/boyerMoore.service.spec.ts
+++ advances-search-app/src/app/boyerMoore.service.spec.ts
@@ -22,6 +22,53 @@ describe('boyerMoorePatternService', () => {
 
                 expect(exist).toBe(1);
             });
+            it('find test on text', () => {
+                const searchText = 'test';
+                const text = 'this is a test';
+                const exist = service.bruteForce(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+
+            it('find text on text', () => {
+                const searchText = 'text';
+                const text = 'alltexttogether';
+                const exist = service.bruteForce(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+
+            it('find Visa on text', () => {
+                const searchText = 'Visa';
+                const text = 'paywithVisa';
+                const exist = service.bruteForce(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+
+            it('find 97 on text', () => {
+                const searchText = '97';
+                const text = 'nº64986xxx - num97';
+                const exist = service.bruteForce(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+
+            it('find Ñ on text', () => {
+                const searchText = 'Ñ';
+                const text = 'nº6498Ñ6xxx - num97';
+                const exist = service.bruteForce(text, searchText);
+
+                expect(exist).toBe(1);
+            });
+
+            it('find 99$ on text', () => {
+                const searchText = '99$';
+                const text = 'pay 9.099$';
+                const exist = service.bruteForce(text, searchText);
+
+                expect(exist).toBe(1);
+            });
 
 
         });
@@ -67,16 +114,13 @@ describe('boyerMoorePatternService', () => {
                 expect(exist).toBe(1);
             });
 
-            it('find $ on text', () => {
-                const searchText = '97';
+            it('find 99$ on text', () => {
+                const searchText = '99$';
                 const text = 'pay 9.099$';
                 const exist = service.badMatchTable(text, searchText);
 
                 expect(exist).toBe(1);
             });
         });
-
-
-
     });
 });
